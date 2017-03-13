55e765f784cb8f1baaee514bc02e91a564476e0f
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-03-13 18:39:08 -0500

    created database
    can retreive and add data to database manually.

--- server.js
+++ server.js
@@ -2,8 +2,45 @@ var express = require('express')
 var app = express()
 var static = require('express-static');
 var port = process.env.PORT || 3000;
-// app.use(static(__dirname + '/public'));
+var mongodb = require('mongodb').MongoClient;
+var connectionURL = 'mongodb://user:12345@ds145289.mlab.com:45289/haikudb'
 
+//connect to mongodb
+mongodb.connect(connectionURL, function(err, db){
+
+  function getData(){
+   db.collection("haikus", function(err, collection){
+    collection.find().toArray(function(err, items){
+      if(err){
+        throw err;
+      }
+      console.log(items);//displays all items in the db
+    })
+   })
+  }
+
+  //add data to db
+  function addData(name, title, body, date){
+    db.collection("haikus", function(err, collection){
+      collection.insert({"name": name, "title": title, "body": body, "date": date});
+    })
+  }
+});
+
+
+
+//to post to db
+
+
+
+
+
+
+
+
+
+
+//handle get requests
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/client/index.html')
 })
