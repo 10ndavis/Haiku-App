73237997d6f576cb490f2bfc9c475474ecfecc94
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-03-13 14:28:31 -0500

    commit 2

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
 var static = require('express-static');
-
-app.use(static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
+// app.use(static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/client/index.html')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
