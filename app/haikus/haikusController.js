297c1ac3b8eb922e9df6133d2116369e9db32dcb
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-03-14 15:20:10 -0500

    posts working

--- app/haikus/haikusController.js
+++ app/haikus/haikusController.js
@@ -1,4 +1,4 @@
-//var syllable = require('syllable');
+
 
 var app = angular.module('myApp', []);
 app.controller('haikuCtrl', function($scope, $interval, $http) {
@@ -39,8 +39,28 @@ app.controller('haikuCtrl', function($scope, $interval, $http) {
       })
   }
 
+  $scope.checkLegalFive = function(line){
+    if(sylCheck(line) !== 5){
+      console.log("this has to be five");
+  }
+}
+
+  $scope.checkLegalSeven = function(line){
+    if(sylCheck(line) !== 7){
+      console.log("this has to be seven");
+    }
+  }
 
 
 
+
+
+    function sylCheck(word) {
+    word = word.toLowerCase();
+    if(word.length <= 3) { return 1; }
+      word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+      word = word.replace(/^y/, '');
+      return word.match(/[aeiouy]{1,2}/g).length;
+    }
 });
 
